5c2b9f5ca04df1dd6cd2e0cda5a6654ae2aebad5
Yuji Sode <yuji.sode@gmail.com>
2018-08-17 19:44:07 +0900

    Add files via upload

--- landformPath_sample.js
+++ landformPath_sample.js
@@ -1,109 +1,21 @@
 //Yuji SODE 2018
-//sample script using landformPath.js
+//sample script using "landformPath.js" and "landformPath_tools.js"
 //###### code example ######
+//canvas size: 300x300
 var C=window.document.getElementById('sample1'),
-	r=[1,3,5,7,11,13,17,19],
-	R=[],i=0;
+	R=[],i=0,L=getLandform(C);
+//1st path
 while(i<20){R.push(20*Math.random()),i+=1;}
-var L=getLandform(C);
-L.curves(30,30,R,10,5);
-//L.lines(70,70,r,10,3,1);
-//##########################
-function getLandform(canvas){
-	// - canvas: a canvas element
-	var F=function(){return F.R.length;};
-	F.X=[],F.Y=[],F.R=[];
-	//method that resets parameters
-	F.reset=function(){
-		F.X=[],F.Y=[],F.R=[];
-	};
-	//method that generates landform contour lines using curved paths and returns modified radii
-	F.curves=function(x,y,radius,linesN,dN,N){
-		// - x and y: values of coordinates for the center
-		// - radius: an array of radii
-		// - linesN: the total number of contour lines
-		// - dN: a span between two red lines
-		// - N: an optional number of contour lines above the top red line
-		//   0 is default value
-		var ctx=canvas.getContext('2d'),arr=radius,arr2=[],i=0,P=[],_P=[],n=0;
-		linesN=Math.floor(Math.abs(linesN));
-		dN=Math.floor(Math.abs(dN));
-		N=!N?0:-Math.floor(Math.abs(N));
-		ctx.strokeStyle="#000";
-		ctx.lineWidth=1.0;
-		ctx.beginPath();
-		while (i<linesN) {
-			//contour line to color
-			if(!((i+N)%dN!=0)){
-				arr2.push(i!=0?P:arr);
-			}
-			//when i=(linesN-1)
-			if(!(i<linesN-1)){
-				F.X.push(x);
-				F.Y.push(y);
-				_P=linesN>1?P.map(a=>a):arr.map(a=>a);
-				F.R.push(_P.sort((a,b)=>b-a)[0]);
-			}
-			P=i>0?ctx.landformPathCurves(x,y,P,5):ctx.landformPathCurves(x,y,arr,5);
-			i+=1;
-		}
-		ctx.stroke();
-		n=arr2.length;
-		i=0;
-		while(i<n){
-			ctx.strokeStyle="#f00";
-			ctx.lineWidth=1.0;
-			ctx.beginPath();
-			ctx.landformPathCurves(x,y,arr2[i],0);
-			ctx.stroke();
-			i+=1;
-		}
-		arr2=i=_P=n=null;
-		return P;
-	};
-	//method that generates landform contour lines using linear paths and returns modified radii
-	F.lines=function(x,y,radius,linesN,dN,N){
-		// - x and y: values of coordinates for the center
-		// - radius: an array of radii
-		// - linesN: the total number of contour lines
-		// - dN: a span between two red lines
-		// - N: an optional number of contour lines above the top red line
-		//   0 is default value
-		var ctx=canvas.getContext('2d'),arr=radius,arr2=[],i=0,P=[],_P=[],n=0;
-		linesN=Math.floor(Math.abs(linesN));
-		dN=Math.floor(Math.abs(dN));
-		N=!N?0:-Math.floor(Math.abs(N));
-		ctx.strokeStyle="#000";
-		ctx.lineWidth=1.0;
-		ctx.beginPath();
-		while (i<linesN) {
-			//contour line to color
-			if(!((i+N)%dN!=0)){
-				arr2.push(i!=0?P:arr);
-			}
-			//when i=(linesN-1)
-			if(!(i<linesN-1)){
-				F.X.push(x);
-				F.Y.push(y);
-				_P=linesN>1?P.map(a=>a):arr.map(a=>a);
-				F.R.push(_P.sort((a,b)=>b-a)[0]);
-			}
-			P=i>0?ctx.landformPathLines(x,y,P,5):ctx.landformPathLines(x,y,arr,5);
-			i+=1;
-		}
-		ctx.stroke();
-		n=arr2.length;
-		i=0;
-		while(i<n){
-			ctx.strokeStyle="#f00";
-			ctx.lineWidth=1.0;
-			ctx.beginPath();
-			ctx.landformPathLines(x,y,arr2[i],0);
-			ctx.stroke();
-			i+=1;
-		}
-		arr2=i=_P=n=null;
-		return P;
-	};
-	return F;
-}
+L.curves(70,70,R,10,5);
+//2nd path
+R=[],i=0;
+while(i<30){R.push(10*Math.random()),i+=1;}
+L.lines(180,120,R,11,5,1);
+//3rd path
+R=[],i=0;
+while(i<20){R.push(30*Math.random()),i+=1;}
+L.curves(80,190,R,7,5,2);
+//4th path
+R=[],i=0;
+while(i<40){R.push(25*Math.random()),i+=1;}
+L.coveringCurves(R,10,5);
