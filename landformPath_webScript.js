8af5f2464fba651a6ef560334673b7657afb9d39
Yuji Sode <yuji.sode@gmail.com>
2018-08-28 09:30:13 +0900

    Add files via upload

--- landformPath_webScript.js
+++ landformPath_webScript.js
@@ -11,22 +11,27 @@
 (function(){
 	var slf=window.document,SS,C=slf.getElementById('landformPathCvs'),Log=slf.getElementById('initialValues'),
 		L=getLandform(C),SS=capturingCanvas(C,slf.getElementById('downloadDiv')),
-		X=0,Y=0,Fix=!1,Randomness=0,Points=0,R=[],i=0,ctx;
+		X=0,Y=0,Fix=!1,GenArt=!1,Randomness=0,Points=0,R=[],i=0,ctx;
 	//### form event ###
 	slf.getElementById('landformPathForm').addEventListener('change',function(){
 		//if it fixes coordinates
 		Fix=slf.getElementById('fixed').checked;
+		//generative art mode
+		GenArt=slf.getElementById('genArtMode').checked;
 		//center coordinates
 		X=!Fix?Math.floor(C.width*Math.random()):X;
 		Y=!Fix?Math.floor(C.height*Math.random()):Y;
 		Randomness=slf.getElementById('randomness').value;
 		Points=slf.getElementById('points').value;
 		i=0,R=[];
+		if(!GenArt){
+			//clearing log and canvas
+			Log.value='';
+			ctx=C.getContext('2d'),ctx.clearRect(0,0,C.width,C.height),ctx=null;
+		}
 		//initial values
 		while(i<Points){R.push(Randomness*Math.random()),i+=1;}
 		Log.value+=!Log.value?'@'+R.join():'\n@'+R.join();
-		//landforms
-		ctx=C.getContext('2d'),ctx.clearRect(0,0,C.width,C.height),ctx=null;
 		L.curves(X,Y,R,10,5);
 	},false);
 	//### event for capturing canvas ###
